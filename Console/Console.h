ba7c1956c4e0d413b8c05746a4b7bc2c9923033e
liu shaoran <shawn134679@outlook.com>
2022-10-10 17:30:34 +0800

    s

--- Console/Console.h
+++ Console/Console.h
@@ -1,21 +1,29 @@
 /*
   copyright shawn 2022
   license MIT
-  version v1.0
+  version v1.1
 */
 
 #ifndef CONSOLE_H_
 #define CONSOLE_H_
+#include <cassert>
 #include <iostream>
 #include <windows.h>
 #define ESC "\x1B"
 #define CSI "\x1B["
 
+struct Color {
+  int r;
+  int g;
+  int b;
+}back_color = { 0,0,0 }, fore_color = { 255,255,255 };
 inline void Clear(int n = 256) {
   std::cout << CSI"0G";
   std::cout << CSI"0d";
   std::cout << CSI << n << 'M';
 }
+inline Color GetBackColor() { return back_color; }
+inline Color GetForeColor() { return fore_color; }
 inline void Init() {
   std::ios::sync_with_stdio(false);
   DWORD mode = 0;
@@ -23,13 +31,29 @@ inline void Init() {
   mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
   SetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), mode);
 }
+inline void SetBackColor(Color c) { SetBackColor(c.r, c.g, c.b); }
 inline void SetBackColor(int r, int g, int b) {
+  assert(r >= 0);
+  assert(r <= 255);
+  assert(g >= 0);
+  assert(g <= 255);
+  assert(b >= 0);
+  assert(b <= 255);
   std::cout << CSI"48;2;" << std::hex << r << ';' << g << ';' << b << 'm';
   std::cout.setf(std::ios_base::basefield);
+  back_color = { r,g,b };
 }
+inline void SetForeColor(Color c) { SetForeColor(c.r, c.g, c.b); }
 inline void SetForeColor(int r, int g, int b) {
+  assert(r >= 0);
+  assert(r <= 255);
+  assert(g >= 0);
+  assert(g <= 255);
+  assert(b >= 0);
+  assert(b <= 255);
   std::cout << CSI"38;2;" << std::hex << r << ';' << g << ';' << b << 'm';
   std::cout.setf(std::ios_base::basefield);
+  fore_color = { r,g,b };
 }
 
 #undef ESC
